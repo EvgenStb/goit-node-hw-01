2933e23801fe37d9e914595638be6ce9d32cf3e5
Evgeniy Strelbitskiy <evgenstb@gmail.com>
2023-05-20 11:51:04 +0300

    add try_catch

--- contacts.js
+++ contacts.js
@@ -4,44 +4,67 @@ const path = require("path");
 const contactsPath = path.join(__dirname, "/db/contacts.json");
 
 async function listContacts() {
-  const contacts = await fs.readFile(`${contactsPath}`, "utf-8");
-  return JSON.parse(contacts);
+  try {
+    const contacts = await fs.readFile(`${contactsPath}`, "utf-8");
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
 
 async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contact = contacts.find((item) => item.id === contactId);
-  return contact || null;
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find((item) => item.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
 
 async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return console.log("This contacts not found");
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return console.log("This contacts not found");
+    }
+    const [deletedContact] = contacts.splice(index, 1);
+    await fs.writeFile(`${contactsPath}`, JSON.stringify(contacts, null, +1));
+    return deletedContact;
+  } catch (error) {
+    console.log(error)
   }
-  const [deletedContact] = contacts.splice(index, 1);
-  await fs.writeFile(`${contactsPath}`, JSON.stringify(contacts, null, +1));
-  return deletedContact;
+  
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const newContact = {
-    id: uuidv4(),
-    name,
-    email,
-    phone,
-  };
-  const duplicateContact = contacts.findIndex(
-    (item) => item.name.toLowerCase() === name.toLowerCase()
-  );
-  if (duplicateContact !== -1) {
-    return console.log("This contact is alredy in contacts");
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+    const duplicateContact = contacts.findIndex(
+      (item) => item.name.toLowerCase() === name.toLowerCase()
+    );
+    if (duplicateContact !== -1) {
+      return console.log("This contact is alredy in contacts");
+    }
+    const newContactsObj = [...contacts, newContact];
+    await fs.writeFile(
+      `${contactsPath}`,
+      JSON.stringify(newContactsObj, null, +1)
+    );
+    return newContact;
+  } catch (error) {
+    console.log(error)
   }
-  const newContactsObj = [...contacts, newContact]
-  await fs.writeFile(`${contactsPath}`, JSON.stringify(newContactsObj, null, +1));
-  return newContact;
+  
 }
 
 
--- index.js
+++ index.js
@@ -43,3 +43,5 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 };
 
 invokeAction(argv);
+
+
